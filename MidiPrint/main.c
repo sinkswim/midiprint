ce53f77102e8cb4562333ea3fea2e3ce3db4523f
robertmargelli <margellirobert@gmail.com>
2015-04-01 12:21:14 -0700

    added full device listing and runloop for all sources

--- MidiPrint/main.c
+++ MidiPrint/main.c
@@ -47,7 +47,29 @@ int main(int argc, const char * argv[]) {
 //    }
     
     ItemCount numOfDevices = MIDIGetNumberOfDevices();
-    
+    /////
+    ItemCount numSources = MIDIGetNumberOfSources();
+    printf("number of sources: %lu\n\n", numSources);
+    MIDIClientRef midiClients[numSources];
+    MIDIPortRef inputPorts[numSources];
+    MIDIEndpointRef endpoints[numSources];
+    OSStatus result;
+    for(int i = 0; i < numSources; i++){
+            result = MIDIClientCreate(CFSTR("MIDI client"), NULL, NULL, &midiClients[i]);
+            if (result != noErr) {
+                printf("Error creating MIDI client: %s - %s", GetMacOSStatusErrorString(result), GetMacOSStatusCommentString(result));
+                return 1;
+            }
+            result = MIDIInputPortCreate(midiClients[i], CFSTR("Input"), midiInputCallback, NULL, &inputPorts[i]);
+            endpoints[i] = MIDIGetSource(i);
+            result = MIDIPortConnectSource(inputPorts[i], endpoints[i], NULL);
+            if (result != noErr) {
+                printf("Error connecting port and source: %s - %s", GetMacOSStatusErrorString(result), GetMacOSStatusCommentString(result));
+                return 1;
+            }
+
+    }
+    //////
         for (int i = 0; i < numOfDevices; i++) {
             MIDIDeviceRef midiDevice = MIDIGetDevice(i);
             CFStringRef deviceName = getDisplayName(midiDevice);
@@ -76,7 +98,7 @@ int main(int argc, const char * argv[]) {
 //        return 1;
 //    }
     
-   // CFRunLoopRun();
+   CFRunLoopRun();
     
     return 0;
 }
